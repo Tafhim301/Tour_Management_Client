071fd910fa52e89ca7f531e74ecbf40d5c40acb4
Tafhim301 <tafhim301@gmail.com>
2025-08-16 23:04:19 +0600

    updated gitignore

--- src/components/modules/Authentication/LoginForm.tsx
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -25,6 +25,7 @@ export function LoginForm({
     try {
       const res = await login(data).unwrap();
       console.log(res);
+      console.log(data)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err : any) {
       console.error(err);
